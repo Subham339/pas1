7473d7fc6f564886668b0b5b13f5ffabfc2831aa
Subham339 <subhambanik554@gmail.com>
2024-07-25 18:47:43 +0530

    defect fixed - 3578 - Fixed null values not to be stored in database

---
+++ src/main/java/com/git_example/git_example/A.java
@@ -0,0 +1,10 @@
+package com.git_example.git_example;
+
+public class A {
+    public void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+    public static void manin(String args[]){
+        System.out.println("Hello World");
+    }
+}
